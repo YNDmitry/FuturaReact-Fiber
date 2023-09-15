f6f47c05307d0e99e257ac10e4b171f6abf33651
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2023-09-15 17:23:45 +0500

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Hero from './Hero.jsx'
-import Contact from './Contact.jsx'
 
 ReactDOM.createRoot(document.getElementById('hero-canvas')).render(<Hero />)
-ReactDOM.createRoot(document.getElementById('contact-canvas')).render(<Contact />)
