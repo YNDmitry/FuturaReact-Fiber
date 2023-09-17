0aa1046d08074fe20f1f6c601337b2d8e9a7d792
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2023-09-17 21:57:36 +0500

    updates

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import gsap from 'gsap'
 import Diamond from './components/Diamond'
 import { Perf } from 'r3f-perf'
@@ -8,28 +8,33 @@ import { useInView } from '@react-spring/web'
 
 function CurrentDiamond() {
   const ref = useRef()
-  const position = [1.5, 0, 0.2]
+  const position = [0, 0, -0.5]
   const rotation = [2, 0, -0.2]
 
   if (window.innerWidth <= 991) {
     position[0] = 0
   }
-  const { viewport } = useThree()
-  useFrame(({ mouse }) => {
+  let mouse = {
+    x: 0,
+    y: 0
+  }
+  document.addEventListener('mousemove', (e) => {
+    mouse.x = e.x
+    mouse.y = e.y
+  })
+  useFrame(() => {
     if (window.innerWidth <= 991) {
       ref.current.rotation.y += 0.01
     } else {
-      const x = (mouse.x * viewport.width) / 20
-      const y = (mouse.y * viewport.height) / 20
+      const x = mouse.x / window.innerWidth
+      const y = mouse.y / window.innerHeight
       gsap.to(ref.current.position, {
         x: x + position[0],
-        y: y + position[1],
-        z: position[2],
         duration: 1.5
       })
       gsap.to(ref.current.rotation, {
-        y: rotation[2] + mouse.x * 2,
-        x: rotation[0] + -mouse.y / 2
+        y: rotation[2] + x * 2,
+        x: rotation[0] + y - 0.8
       })
     }
   })
@@ -52,8 +57,8 @@ function HeroScreen() {
   const [ref, inView] = useInView()
 
   return (
-    <div ref={ref} style={{ height: '100vh' }}>
-      <Canvas flat linear camera={{ position: [0, -4, 0], fov: 45 }}>
+    <div ref={ref} style={{ height: '100%' }}>
+      <Canvas flat linear camera={{ position: [0, -3, 0], fov: 45 }}>
         {isDev ? <Perf position="top-left" /> : ''}
         <ambientLight />
         <pointLight position={[1.5, 0, 0.2]} />
